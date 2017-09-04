238a312f0ba4305cabb1252fad9b0ee6bc32dfb1
xiaodong <pengwao@163.com>
2017-09-04 18:09:41 +0800

    12

---
+++ taotao-search-web/src/main/java/com/taotao/search/controller/SearchController.java
@@ -0,0 +1,41 @@
+package com.taotao.search.controller;
+
+import com.taotao.pojo.SearchResult;
+import com.taotao.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+public class SearchController {
+    @Autowired
+    private SearchService searchService;
+
+    @Value("${ITEM_ROS}")
+    private Integer ITEM_ROWS;
+
+    public String search(@RequestParam("q") String queryString, @RequestParam(defaultValue = "1") Integer page, Model model) {
+
+        try {
+            SearchResult search = searchService.search(queryString, page, ITEM_ROWS);
+
+            model.addAttribute("query",queryString);
+
+            model.addAttribute("totalPages",search.getPageCount());
+
+            model.addAttribute("itemList",search);
+
+            model.addAttribute("page",page);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+
+        }
+
+        return "search";
+    }
+
+
+}
